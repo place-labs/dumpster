7698adf1ec0e2df165858fa3e13a7bcdd6cef131
Kim Burgess <kim@acaprojects.com>
2019-07-15 15:41:56 +1000

    adjust formatting based on crystal standard

--- src/dumpster/entry.cr
+++ src/dumpster/entry.cr
@@ -2,6 +2,8 @@ require "json"
 require "./entry/*"
 
 module Dumpster::Entry
+  extend self
+
   # Character offset for the start of the "type" field (for non-root entries).
   VT_START_POS = 37
 
@@ -10,12 +12,12 @@ module Dumpster::Entry
                 Entry::Class
 
   # Parse an single line of a mem dump into its associated entry struct.
-  def self.parse(line : String) : Types?
+  def parse(line : String) : Types?
     type_of(line).try &.from_json(line)
   end
 
   # Infer the type of a line based on it's raw String form.
-  def self.type_of(line : String)
+  def type_of(line : String)
     case line[VT_START_POS]
     when 'O' then Entry::Object
     when 'C' then Entry::Class
--- src/dumpster/entry/class.cr
+++ src/dumpster/entry/class.cr
@@ -4,7 +4,7 @@ struct Dumpster::Entry::Class
   include Common
 
   JSON.mapping(
-    address: { setter: false, type: UInt64, converter: Address                },
-    name:    { setter: false, type: String?                                   }
+    address: {setter: false, type: UInt64, converter: Address},
+    name: {setter: false, type: String?}
   )
 end
