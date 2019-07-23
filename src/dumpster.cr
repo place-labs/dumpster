b019c6532a67a124fac26f02126f4606cecd7d60
Kim Burgess <kim@acaprojects.com>
2019-07-23 22:45:53 +1000

    neaten up conditional run when executing specs

--- src/dumpster.cr
+++ src/dumpster.cr
@@ -1,5 +1,5 @@
 require "./dumpster/cli"
 
-running_specs = PROGRAM_NAME.ends_with? "crystal-run-spec.tmp"
+RUNNING_SPECS = PROGRAM_NAME.ends_with? "crystal-run-spec.tmp"
 
-Dumpster::Cli.run unless running_specs
+Dumpster::Cli.run unless RUNNING_SPECS
