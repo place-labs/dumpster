4b544e6aed3c574232024d83c9365d07a8cc454f
Kim Burgess <kim@acaprojects.com>
2019-07-23 15:23:43 +1000

    add generation and location extraction for array, hash and structs

--- src/dumpster/entry/struct.cr
+++ src/dumpster/entry/struct.cr
@@ -4,6 +4,14 @@ struct Dumpster::Entry::Struct
   include Common
 
   JSON.mapping(
-    address: {setter: false, type: UInt64, converter: Address}
+    address: {setter: false, type: UInt64, converter: Address},
+    file: {setter: false, type: ::String?},
+    line: {setter: false, type: UInt32?},
+    generation: {setter: false, type: UInt32?}
   )
+
+  # Returns the location as "file:line" where this instance was instantiated.
+  def location
+    "#{file}:#{line}" if file && line
+  end
 end
