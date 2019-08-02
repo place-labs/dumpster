f0e96c6b38898842ca2abec45d7cbe3a7007dca9
Kim Burgess <kim@acaprojects.com>
2019-08-02 10:28:23 +1000

    use Crystal.main block for execution

--- src/dumpster.cr
+++ src/dumpster.cr
@@ -1,5 +1,6 @@
 require "./dumpster/cli"
 
+# FIXME: find a less hacky method for doing this
 RUNNING_SPECS = PROGRAM_NAME.ends_with? "crystal-run-spec.tmp"
 
-Dumpster::Cli.run unless RUNNING_SPECS
+Crystal.main &->Dumpster::Cli.run unless RUNNING_SPECS
