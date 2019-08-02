d2654f202b4451844d7a63e603acb8be08fc4b64
Kim Burgess <kim@acaprojects.com>
2019-08-02 22:48:54 +1000

    support stats calculations for both sample and population sources

--- spec/lib/math/stats.cr
+++ spec/lib/math/stats.cr
@@ -2,14 +2,18 @@ require "../../spec_helper"
 
 describe Enumerable do
   describe "#mean" do
-    it { [4, 36, 45, 50, 75].mean.should be_close(42, 1e-10) }
+    it { [4, 36, 45, 50, 75].mean.should eq(42) }
+    it { (1..6).mean.should eq(3.5) }
   end
 
-  describe "#variance" do
-    it { (1..6).variance.should be_close(2.92, 1e-2) }
+  describe "#var" do
+    it { (1..6).var.should be_close(2.917, 1e-3) }
+    it { (1..6).var(ddof: 1).should eq(3.5) }
   end
 
-  describe "#stddev" do
-    it { [2, 4, 4, 4, 5, 5, 7, 9].stddev.should eq(2) }
+  describe "#std" do
+    it { [2, 4, 4, 4, 5, 5, 7, 9].std.should eq(2) }
+    it { (1..6).std.should be_close(1.708, 1e-3) }
+    it { (1..6).std(ddof: 1).should be_close(1.871, 1e-3) }
   end
 end
--- src/lib/math/stats.cr
+++ src/lib/math/stats.cr
@@ -1,20 +1,38 @@
 require "math"
 
-# Extensions for the `Enumerable` module to support common numerical stats
-# functions for numeric collections.
+# Extensions for the `Enumerable` module to support common stats methods.
 module Math::Stats
+  # Compute the mean for values contained within the collection.
+  #
+  # ```
+  # (1..6).mean # => 3.5
+  # ```
   def mean
     sum / size.to_f
   end
 
-  def variance
+  # Compute the variance across values contained in the collection.
+  #
+  # Default normalisation (N) is applied such that this may be used directly on
+  # collections representing the full population. When working with a sample,
+  # *ddof* may be used to specify a delta degrees of freedom and correct bias.
+  #
+  # ```
+  # (1..6).var # => 2.917
+  # ```
+  def var(ddof = 0.0)
     return 0.0 if size == 0
-    x_bar = mean
-    map { |x| (x - x_bar) ** 2 }.mean
+    u = mean
+    sum { |x| (x - u) ** 2 } / (size - ddof)
   end
 
-  def stddev
-    Math.sqrt variance
+  # Compute the standard deviation for values within the collection.
+  #
+  # ```
+  # (1..6).std # => 1.708
+  # ```
+  def std(ddof = 0.0)
+    Math.sqrt var(ddof)
   end
 end
 
